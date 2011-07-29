5c3250c01ee8b015d4a971506574a2d0ee8baab7
Casey <cam@uptoisomorphism.net>
2011-07-28 21:42:29 -0400

    bunch of fiddly nitpicking style changes

--- Main.hs
+++ Main.hs
@@ -15,7 +15,7 @@ import qualified Graphics.Rendering.OpenGL.GL as GL
 main :: IO ()
 main = do SDL.init [SDL.InitTimer, SDL.InitVideo]
           SDL.setVideoMode 800 800 32 [SDL.OpenGL]
-          gameLoop =<< return newGame
+          gameLoop newGame
           SDL.quit
           
 -- | Reads all pending events from SDL's queue.
@@ -30,17 +30,21 @@ keypresses _ = Nothing
 
 gameLoop :: Game -> IO ()
 gameLoop gs = do evs <- newEvents
-                 let gs' = updateGame (catMaybes $ keypresses <$> evs) gs
+                 let gs' = updateGame (mapMaybe keypresses evs) gs
                  GL.clear [GL.ColorBuffer]
                  clearRender $ renderGame gs'
                  SDL.glSwapBuffers
                  unless (any (== SDL.Quit) evs) (SDL.delay 20 >> gameLoop gs')
                  
-data Terrain = Dirt | Wall
-data Level = Level { size :: Int,
-                     terrain :: [[Terrain]] }
-data Game = Game { player :: (Int,Int),
-                   level :: Level }
+data Terrain = Dirt | Wall deriving (Eq, Ord, Show, Read)
+
+data Level = Level { size    :: Int
+                   , terrain :: [[Terrain]] 
+                   } deriving (Eq, Ord, Show, Read)
+
+data Game = Game { player :: (Int,Int)
+                 , level  :: Level 
+                 } deriving (Eq, Ord, Show, Read)
 
 newGame :: Game
 newGame = Game (0,0) (emptyLevel 9)
@@ -49,10 +53,13 @@ emptyLevel :: Int -> Level
 emptyLevel sz = Level sz (replicate sz (replicate sz Dirt))
 
 updateGame :: [SDLKey] -> Game -> Game
-updateGame ks g = g { player = clamp  (size $ level g) $  foldr movePlayer (player g) (getMoves ks) }
-                  where clamp n (x,y) = (max 0 $ min (n-1) x, max 0 $ min (n-1) y) --oh god
-                        getMoves = catMaybes . map getMove
-                        
+updateGame ks g = g { player = clampPlayer $ foldr movePlayer (player g) playerMoves }
+  where clampToLevel = clamp 0 (subtract 1 . size $ level g)
+        clampPlayer (x,y) = (clampToLevel x, clampToLevel y)
+        playerMoves = mapMaybe getMove ks
+
+clamp lb ub = max lb . min ub
+
 getMove :: SDLKey -> Maybe (Int,Int)
 getMove SDLK_DOWN  = Just ( 0, 1)
 getMove SDLK_UP    = Just ( 0,-1)
@@ -64,11 +71,17 @@ movePlayer :: (Int,Int) -> (Int,Int) -> (Int,Int)
 movePlayer (x1,y1) (x2,y2) = (x1+x2, y1+y2)
 
 renderGame :: Game -> Image Any
-renderGame g = translate(-1,1) %% scale (2/ (fromIntegral $ size $ level g)) (-2/ (fromIntegral $ size $ level g)) %% doCrap
+renderGame g = translate(-1,1) 
+           %% uncurry scale (divideR2 (2, -2) . toDamnR2 $ levelDim g)
+           %% doCrap
   where doCrap = drawPlayer
-        drawPlayer = (translate $ toDamnR2 $ player g) 
-                     %% tint (Color 0 1 0 1) (convexPoly [(0,0), (0,1), (1,1), (1,0)])
+        drawPlayer = (translate . toDamnR2 $ player g) 
+                  %% tint (Color 0 1 0 1) (convexPoly [(0,0), (0,1), (1,1), (1,0)])
+
+levelSize = size . level
+levelDim g = (levelSize g, levelSize g)
 
 toDamnR2 :: Integral a => (a,a) -> R2
 toDamnR2 (x,y) = (fromIntegral x, fromIntegral y)
 
+divideR2 (x1, y1) (x2, y2) = (x1 / x2, y1 / y2)
